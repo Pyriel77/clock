22d6a8eafa4ca3a07ee6680f76f227a886763a53
Arpad Nyircsak <69168570+Pyriel77@users.noreply.github.com>
2023-10-01 16:22:51 +0200

    buttons added

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const hourNeedle = document.querySelector('[hour-needle]');
 const minNeedle = document.querySelector('[minute-needle]');
 const secNeedle = document.querySelector('[second-needle]');
+const minusButton = document.querySelector('.minus');
+const plusButton = document.querySelector('.plus');
+
+let modifier = 0;
 
 const setClock = () => {
   const now = new Date();
-  const sec = now.getSeconds() / 60;
-  const min = (sec + now.getMinutes()) / 60;
-  const hour = (min + now.getHours()) / 12;
+  let alterTime = new Date();
+  modifier = Number(localStorage.getItem('mod'));
+  alterTime.setTime(now.getTime() + modifier);
+  console.log(alterTime);
+  const sec = alterTime.getSeconds() / 60;
+  const min = (sec + alterTime.getMinutes()) / 60;
+  const hour = (min + alterTime.getHours()) / 12;
   setRotation(secNeedle, sec);
   setRotation(minNeedle, min);
   setRotation(hourNeedle, hour);
@@ -15,6 +23,18 @@ const setClock = () => {
 const setRotation = (elem, rotatio) => {
   elem.style.setProperty('--rot', rotatio * 360);
 };
+
+const modify = (mod) => {
+  modifier += mod * 60000;
+  localStorage.setItem('mod', modifier);
+};
+
+minusButton.addEventListener('click', () => {
+  modify(-10);
+});
+plusButton.addEventListener('click', () => {
+  modify(5);
+});
 
 setInterval(setClock, 1000);
 setClock();
